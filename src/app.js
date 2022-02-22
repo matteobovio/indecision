777f779403a467c592cfd2570d37d1043a930fdb
Matteo Bovio <matteo@zalando.it>
2022-02-22 12:29:20 +0100

    test import React

--- src/app.js
+++ src/app.js
@@ -1,18 +1,5 @@
-// console.log('app.js is running!!');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import subtract, { square, add } from './utils.js';
-
-// console.log(square(4));
-// console.log(add(12, 49));
-// console.log(subtract(10, 6));
-
-// import { isAdult, canDrink } from './person.js';
-// import isSenior from './person.js';
-
-// console.log(isAdult(4));
-// console.log(canDrink(18));
-// console.log(isSenior(65));
-
-import validator from 'validator';
-
-console.log(validator.isEmail('test@gmail.com'));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
