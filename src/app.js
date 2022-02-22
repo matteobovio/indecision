27b1ad27c33c9d4101f3e7fb5a391518eb3d591f
Matteo Bovio <matteo@zalando.it>
2022-02-22 12:16:24 +0100

    test add npm modules

--- src/app.js
+++ src/app.js
@@ -6,9 +6,13 @@
 // console.log(add(12, 49));
 // console.log(subtract(10, 6));
 
-import { isAdult, canDrink } from './person.js';
-import isSenior from './person.js';
+// import { isAdult, canDrink } from './person.js';
+// import isSenior from './person.js';
 
-console.log(isAdult(4));
-console.log(canDrink(18));
-console.log(isSenior(65));
+// console.log(isAdult(4));
+// console.log(canDrink(18));
+// console.log(isSenior(65));
+
+import validator from 'validator';
+
+console.log(validator.isEmail('test@gmail.com'));
