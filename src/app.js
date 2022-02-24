6b19b0e5025f438a277be1fc57961e4122a71398
Matteo Bovio <matteobovio@gmail.com>
2022-02-25 00:52:23 +0100

    add sass support

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-
-const Layout = (props) => {
-    return (
-        <div>
-            <p>Header</p>
-            {props.children}
-            <p>Footer</p>
-        </div>
-    );
-};
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
