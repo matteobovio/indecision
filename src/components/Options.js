1ece3c3f964b4d1f11dba255990c13d8a0219039
Matteo Bovio <matteo@zalando.it>
2022-03-01 22:43:55 +0100

    styling buttons

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,7 +3,12 @@ import Option from './Option';
 
 const Options = (props) => (
     <div>
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        <button 
+            onClick={props.handleDeleteOptions}
+            className="button button--link"
+        >
+            Remove All
+        </button>
         {props.options.length === 0 && <p>Please add an option to get started</p>}
         {
             props.options.map((option, index) => (
